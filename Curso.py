937261f9ed3eacd7f328aac06966ad8296433350
MORENOG-2 <rafaelmore846@gmail.com>
2026-03-11 20:35:15 -0500

    Curso

--- Curso.py
+++ Curso.py
@@ -1,1 +1,23 @@
-print("Rafael Moreno")
+# htps://python.org/
+
+"""
+Vamos a comentar 
+en varias líneas
+"""
+
+# Variables y tipos de datos
+RM = "hola"
+RM = "mundo"
+
+int = 25
+float = 3.1415
+bool = True
+bool = False
+str = "hola mundo"
+
+print("¡hola, es python!")
+
+print(type(int))
+print(type(float))
+print(type(bool))
+print(type(str))
