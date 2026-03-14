f7e48477744c00370a5094767e020e045e920ea4
MORENOG-2 <rafaelmore846@gmail.com>
2026-03-14 15:25:29 -0500

    Tercer_Curso

--- Curso03.py
+++ Curso03.py
@@ -1,1 +1,18 @@
 print("Como estan amigos, bienvenidos a la programación")
+
+if 5 < 3:
+    print("5 es menor que 3")
+else:
+    print("5 no es menor que 3")
+is_mayor = 5 > 3
+print(is_mayor)
+is_menor = 5 < 3
+print(is_menor)
+is_igual = 5 == 3
+print(is_igual)
+is_diferente = 5 != 3
+print(is_diferente)
+is_mayor_igual = 5 >= 3
+print(is_mayor_igual)
+is_menor_igual = 5 <= 3
+print(is_menor_igual)
