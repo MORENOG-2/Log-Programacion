e3e6ae7b3bebbad6756b7c000f64f34e092d5d58
MORENOG-2 <rafaelmore846@gmail.com>
2026-03-20 17:53:23 -0500

    TercerCurso

--- Curso03.py
+++ Curso03.py
@@ -1,21 +1,41 @@
-print("Como estan amigos, hoy vamos a aprender sobre operadores de comparación")
+# Ejemplo de craciones de todas las estructuras soportadas en python
 
-if 5 < 3:
-    print("5 es menor que 3")
-else:
-    print("5 no es menor que 3")
-is_mayor = 5 > 3
-print(is_mayor)
-is_menor = 5 < 3
-print(is_menor)
-is_igual = 5 == 3
-print(is_igual)
-is_diferente = 5 != 3
-print(is_diferente)
-is_mayor_igual = 5 >= 3
-print(is_mayor_igual)
-is_menor_igual = 5 <= 3
-print(is_menor_igual)
+# Listas
+lista = [1, 2, 3, 4, 5]
+print(lista)
 
-##dwqef
-##jtrtj
+# Tuplas
+tupla = (1, 2, 3, 4, 5)
+T = tupla, lista
+print(T)
+
+# Diccionarios
+diccionario = {"nombre": "Rafael", "apellido": "Moreno", "edad": 26}
+print(diccionario)
+
+# Conjuntos
+conjunto = {1, 2, 3, 4, 5}
+print(conjunto)
+
+# Funciones
+def saludar(nombre):
+    print(f"Hola {nombre}, bienvenido a la programación")
+saludar("Rafael")
+
+# Clases
+class Persona:
+    def __init__(self, nombre, apellido, edad):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.edad = edad
+
+    def saludar(self):
+        print(f"Hola {self.nombre} {self.apellido}, bienvenido a la programación")
+persona1 = Persona("Rafael", "Moreno", 26)
+persona1.saludar()
+
+# Módulos
+def saludar_modulo(nombre):
+    print(f"Hola {nombre}, bienvenido a la programación")
+saludar_modulo("Rafael")
+
