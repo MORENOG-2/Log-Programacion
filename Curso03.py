7fc5200fe8cc7560486b3ea19631df5a4d2e599d
MORENOG-2 <rafaelmore846@gmail.com>
2026-03-21 10:31:53 -0500

    CuartoCurso

--- Curso03.py
+++ Curso03.py
@@ -3,6 +3,16 @@
 # Listas
 lista = [1, 2, 3, 4, 5]
 print(lista)
+lista.append(6) #Para agregar un elemento a la lista
+print(lista)
+lista.remove(3) #Para eliminar un elemento de la lista
+print(lista)
+lista[1]  #Acceder a un elemento de la lista por su índice
+print(lista[1])
+lista[1] = 10 #Modificar un elemento de la lista por su índice
+print(lista)
+lista.sort() #Ordenar la lista
+print(lista)
 
 # Tuplas
 tupla = (1, 2, 3, 4, 5)
