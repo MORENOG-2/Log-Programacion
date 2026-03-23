7a7ca945d125265ec67859eeb8867322c9317836
MORENOG-2 <rafaelmore846@gmail.com>
2026-03-22 19:45:51 -0500

    TercerCurso

--- Curso03.py
+++ Curso03.py
@@ -65,29 +65,4 @@ diccionario.clear() #Eliminar todos los elementos del diccionario
 print(diccionario)
 print("Fin de diccionarios")
 
-# Conjuntos
-conjunto = {1, 2, 3, 4, 5}
-print(conjunto)
-
-# Funciones
-def saludar(nombre):
-    print(f"Hola {nombre}, bienvenido a la programación")
-saludar("Rafael")
-
-# Clases
-class Persona:
-    def __init__(self, nombre, apellido, edad):
-        self.nombre = nombre
-        self.apellido = apellido
-        self.edad = edad
-
-    def saludar(self):
-        print(f"Hola {self.nombre} {self.apellido}, bienvenido a la programación")
-persona1 = Persona("Rafael", "Moreno", 26)
-persona1.saludar()
-
-# Módulos
-def saludar_modulo(nombre):
-    print(f"Hola {nombre}, bienvenido a la programación")
-saludar_modulo("Rafael")
 
